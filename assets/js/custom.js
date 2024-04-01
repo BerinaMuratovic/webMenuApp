cd0e73c94e03878549b42af26920c5383d02f6d6
Berina Muratovic <berina.muratovic@stu.ibu.edu.ba>
2024-04-01 16:17:02 +0200

    Multiple route changes and fixed issues.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,7 @@
 $(document).ready(function() {
     var app = $.spapp({
-        defaultView: "#view_home", // Default view to load
-        templateDir: "./views/"    // Directory containing view files
-    });
-
-    // Define routes for each view
-    app.route({
-        view: "view_home",
-        load: "home.html",
-        onCreate: function() { },
-        onReady: function() { }
+        defaultView: "#view_home",
+        templateDir: "./views/"
     });
 
     app.route({
@@ -32,6 +24,60 @@ $(document).ready(function() {
         onCreate: function() { },
         onReady: function() { }
     });
+    app.route({
+        view: "view_login",
+        load: "login.html",
+        onCreate: function() { },
+        onReady: function() { }
+    });
 
-    app.run(); // Start the application
+    app.route({
+        view: "view_register",
+        load: "register.html",
+        onCreate: function() { },
+        onReady: function() { }
+    });
+
+    app.run();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const tabLinks = document.querySelectorAll('.tm-tab-link');
+    const navLinks = document.querySelectorAll('.tm-page-link');
+
+
+    tabLinks.forEach(function(tabLink) {
+        tabLink.addEventListener('click', function(event) {
+            event.preventDefault();
+
+
+            const tabId = this.getAttribute('data-id');
+
+
+            document.querySelectorAll('.tm-tab-content').forEach(function(tabContent) {
+                tabContent.style.display = 'none';
+            });
+
+
+            document.getElementById(tabId).style.display = 'block';
+
+
+            tabLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+
+
+            this.classList.add('active');
+        });
+    });
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener('click', function(event) {
+            navLinks.forEach(function(link) {
+                link.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
 });
